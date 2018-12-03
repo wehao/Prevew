925370de48ea89a80cd003e2178d629ee1c586a8
chenweihao <chenweihao@flyaudio.cn>
2018-12-03 11:32:49 +0800

    删除一些文件

--- app/jni/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := ImageProc
-LOCAL_SRC_FILES := ImageProc.c
-LOCAL_LDLIBS := -llog -ljnigraphics
-
-include $(BUILD_SHARED_LIBRARY)
